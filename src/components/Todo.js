955a95cc4b1de370d4651b9f73ae8d7e1dc89c67
Juan Aleman <juan.j.aleman@gmail.com>
2019-10-30 22:07:34 -0500

    completed the todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Todo = props => {
 
+    const handleClick = () => {
+        props.toggleTodo(props.todo.id);
+    };
 
     return(
-        <div>
-            This is a Todo
+        <div onClick = {handleClick} className={`todo${props.todo.completed ? ' completed' : ''}`}>
+            <p>{props.todo.task}</p>
+            
         </div>
-    )
-}
+    );
+};
 
 export default Todo;
